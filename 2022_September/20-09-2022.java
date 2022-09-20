7c7e6f03255766e0590ddca95dfbe5e74631e8ba
Punit pawar <70194383+SkyrunnerOO7@users.noreply.github.com>
2022-09-20 14:55:38 +0530

    1.2

--- 2022_September/20-09-2022.java
+++ 2022_September/20-09-2022.java
@@ -40,3 +40,47 @@ class GFG
 	 
 	 }
 }
+
+
+
+//By using only single index
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+ {
+    static void rev(int arr[], int i, int n)
+    {
+        if(i>=n/2)
+        {return;}
+        
+        //swap
+        int temp = arr[i];
+        arr[i] = arr[n-i-1];
+        arr[n-i-1] = temp;
+        
+        rev(arr, i+1, n);
+    }
+    
+	public static void main (String[] args)
+	 {
+	 //code
+	 Scanner sc = new Scanner(System.in);
+	 int n = sc.nextInt();
+	 int size = sc.nextInt();
+	 int arr[] = new int[size];
+	 
+	 for(int i =0; i<size; i++)
+	 {
+	     arr[i] = sc.nextInt();
+	 }
+	 
+	 rev(arr, 0, size);
+	 
+	 for(int i =0; i<size; i++)
+	 {
+	     System.out.print(arr[i]+" ");
+	 }
+	 
+	 }
+}
